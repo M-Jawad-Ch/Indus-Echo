d563aad9f24788f0b0d46c6433fa13a1d716d4b5
jawad <mjawadch55@gmail.com>
2023-07-06 20:20:48 +0500

    Setting settings.py to production settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,9 +29,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.get_value('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['blog.indusecho.site', '*']
+ALLOWED_HOSTS = ['blog.indusecho.site']
 
 
 # Application definition
